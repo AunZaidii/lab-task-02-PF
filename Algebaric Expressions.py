c762fe4ee2a0b77255268e153bbc0ddb3aaf5622
Aun <148131524+AunZaidii@users.noreply.github.com>
2024-04-27 00:11:34 +0500

    Add files via upload

---
+++ Algebaric Expressions.py
@@ -0,0 +1,19 @@
+sum = 1+2+3+4+5
+print ("The sum of first five positive integers is ",sum)
+sara = 23
+mark = 19 
+fatima = 31
+average = (sara+mark+fatima)/3
+print(average)
+times_into = 403 // 73
+print(times_into)
+remainder = 403 % 73
+print(remainder)
+power = 2**10
+print(power)
+height = abs(54-57)
+print(height)
+lowest_price = min(34.99,29.95,31.50)
+print(lowest_price)
+
+
